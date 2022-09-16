e154c0174d25a149a813d4367e5f7862171cb17e
Vignesh Mani <vignesh@kawa.space>
2022-09-16 15:10:44 +0530

    refactored utils

--- utils/common.go
+++ utils/common.go
@@ -41,24 +41,20 @@ func ReadString(filename string) string {
 
 func MapToInt(strs []string) []int {
 	ints := make([]int, len(strs))
-	i := 0
-	for _, s := range strs {
+	for i, s := range strs {
 		num, err := strconv.Atoi(s)
 		if err != nil {
 			fmt.Println(err)
 		}
 		ints[i] = num
-		i++
 	}
 	return ints
 }
 
 func MapToStr(ints []int) []string {
 	strs := make([]string, len(ints))
-	i := 0
-	for _, num := range ints {
+	for i, num := range ints {
 		strs[i] = strconv.Itoa(num)
-		i++
 	}
 	return strs
 }
@@ -102,7 +98,7 @@ func Contains[T comparable](slice []T, item T) bool {
 }
 
 func Sum[T int | float64](slice []T) T {
-	sum := T(0)
+	var sum T
 	for _, v := range slice {
 		sum += v
 	}
@@ -155,136 +151,4 @@ func IndexOf[T comparable](slice []T, item T) int {
 		}
 	}
 	return -1
-}
-
-type Set[T comparable] map[T]struct{}
-
-func (s *Set[T]) Add(item T) {
-	(*s)[item] = struct{}{}
-}
-
-func (s *Set[T]) Contains(item T) bool {
-	if _, ok := (*s)[item]; ok {
-		return true
-	}
-	return false
-}
-
-func (s *Set[T]) AddAll(slice []T) {
-	for _, v := range slice {
-		(*s).Add(v)
-	}
-}
-
-func (s *Set[T]) RetainAll(slice []T) {
-	for _, v := range MapKeys(*s) {
-		if !Contains(slice, v) {
-			delete(*s, v)
-		}
-	}
-}
-
-type Stack[T any] interface {
-	Empty() bool
-	Peek() T
-	Pop() (T, bool)
-	Push(item T)
-}
-
-type Queue[T any] interface {
-	Empty() bool
-	Peek() T
-	Dequeue() (T, bool)
-	Enqueue(item T)
-}
-
-type node[T any] struct {
-	data T
-	prev *node[T]
-	next *node[T]
-}
-
-type Deque[T any] struct {
-	head   *node[T]
-	tail   *node[T]
-	length int
-}
-
-func NewDeque[T any]() *Deque[T] {
-	var deque Deque[T]
-	return &deque
-}
-
-func (d *Deque[T]) Push(item T) {
-	newNode := &node[T]{
-		data: item,
-		prev: nil,
-		next: nil,
-	}
-
-	if d.length == 0 {
-		d.head = newNode
-		d.tail = newNode
-	} else {
-		newNode.next = d.head
-		d.head.prev = newNode
-		d.head = newNode
-	}
-	d.length++
-}
-
-func (d *Deque[T]) Pop() (T, bool) {
-	if d.length == 0 {
-		var nope T
-		return nope, false
-	} else {
-		temp := d.head
-		d.head = d.head.next
-		if d.head != nil {
-			d.head.prev = nil
-		} else {
-			d.tail = nil
-		}
-		d.length--
-		return temp.data, true
-	}
-}
-
-func (d *Deque[T]) Peek() T {
-	return d.head.data
-}
-
-func (d *Deque[T]) Enqueue(item T) {
-	newNode := &node[T]{
-		data: item,
-		prev: nil,
-		next: nil,
-	}
-
-	if d.length == 0 {
-		d.head = newNode
-		d.tail = newNode
-	} else {
-		newNode.prev = d.tail
-		d.tail.next = newNode
-		d.tail = newNode
-	}
-	d.length++
-}
-
-func (d *Deque[T]) Dequeue() (T, bool) {
-	return d.Pop()
-}
-
-func (d *Deque[T]) Empty() bool {
-	return d.length == 0
-}
-
-func (d *Deque[T]) Clear() {
-	var deque Deque[T]
-	*d = deque
-}
-
-func (d *Deque[T]) Len() int {
-	return d.length
 }
